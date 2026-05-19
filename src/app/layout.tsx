40328387ab2d31b5a2e5119079b64350b7a5d8e4
MarketingCo <INFO@MARKETING-COMPANY.CO.UK>
2026-05-19 21:28:17 +0100

    SEO: set lang=en-GB

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en-GB" className="h-full">
       <head>
         <script
           type="application/ld+json"
